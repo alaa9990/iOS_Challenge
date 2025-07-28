9ea5a3df09b88f93c76f95da30393701c9c05ba6
AlaaHassan70 <alaahmohamed70@gmail.com>
2025-07-28 16:08:27 +0300

    initial commit

---
+++ iOS_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Challenge
+//
+//  Created by Alaa Hassan on 28/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_ChallengeTests/iOS_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChallengeTests.swift
+//  iOS_ChallengeTests
+//
+//  Created by Alaa Hassan on 28/07/2025.
+//
+
+import Testing
+@testable import iOS_Challenge
+
+struct iOS_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
